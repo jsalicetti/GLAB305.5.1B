53b4a65991401742cd061e7c348b20e01c9ff778
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-18 22:04:03 -0400

    changes made for GLAB305.5.4B

--- HibernateMapping/src/main/java/App.java
+++ HibernateMapping/src/main/java/App.java
@@ -1,4 +1,5 @@
 import model.Address;
+import model.Cohort;
 import model.Department;
 import model.Teacher;
 import org.hibernate.Session;
@@ -7,12 +8,15 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class App {
     public static void main(String[] args) {
 
-        oneToOne();
+        manyToMany();
+
     }
 
     public static void manyToOne(){
@@ -71,7 +75,6 @@ public class App {
         session.persist(department);
         t.commit();    }
 
-
     public static void oneToOne(){
         System.out.println("Maven + Hibernate + SQL One to One Mapping Annotations");
 
@@ -84,8 +87,8 @@ public class App {
 
         Teacher t1 = new Teacher("1000","MHaseeb");
         Teacher t2 = new Teacher("2220","Shahparan");
-        t1.setAddress(a1);
-        t2.setAddress(a2);
+        // t1.setAddress(a1);
+        // t2.setAddress(a2);
 
 
 
@@ -94,6 +97,45 @@ public class App {
         session.persist(t1);
         session.persist(t2);
 
+        t.commit();
+    }
+
+    public static void manyToMany(){
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        //----Create Cohort/class Entity set one----
+        Cohort Class1 = new Cohort("Java Developer", "14 weeks");
+        Cohort Class2 = new Cohort("FullStack Developer", "7 Weeks");
+        Cohort Class3 = new Cohort("Python Developer", "12 Weeks");
+        //------  Store Cohort  / Class  --------
+        session.persist(Class1);
+        session.persist(Class2);
+        session.persist(Class3);
+
+        //-----Create Cohort one / Class one --------
+        Set<Cohort> ClassSet1 = new HashSet<Cohort>();
+        ClassSet1.add(Class1);
+        ClassSet1.add(Class2);
+        ClassSet1.add(Class3);
+        //-----Create Cohort two / Class two --------
+        Set<Cohort> ClassSet2 = new HashSet<Cohort>();
+        ClassSet2.add(Class2);
+        ClassSet2.add(Class3);
+        ClassSet2.add(Class1);
+        //-----Create Cohort Three / Class Three --------
+        Set<Cohort> ClassSet3 = new HashSet<Cohort>();
+        ClassSet3.add(Class3);
+        ClassSet3.add(Class1);
+        ClassSet3.add(Class2);
+
+        Teacher t1 = new Teacher("100", "Haseeb", ClassSet1);
+        Teacher t2 = new Teacher("200", "Jenny", ClassSet2);
+        Teacher t3 = new Teacher("200", "Charlie", ClassSet3);
+
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
         t.commit();
     }
 
--- HibernateMapping/src/main/java/model/Teacher.java
+++ HibernateMapping/src/main/java/model/Teacher.java
@@ -1,64 +1,80 @@
+ package model;
+ import jakarta.persistence.*;
+ import java.io.Serial;
+ import java.io.Serializable;
+ import java.util.Set;
 
-       package model;
-
-import jakarta.persistence.*;
-        import model.Department;
-
-import java.io.Serial;
-import java.io.Serializable;
-
-@Entity
-@Table
-public class Teacher implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue( strategy=GenerationType.IDENTITY )
-    private int teacherId;
-    private String salary;
-    private String teacherName;
+ @Entity
+ @Table(name="Teacher")
+ public class Teacher implements Serializable {
+     @Serial
+     private static final long serialVersionUID = 1L;
+     @Id
+     @GeneratedValue( strategy=GenerationType.IDENTITY )
+     private int teacherId;
+     private String salary;
+     private String teacherName;
 
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Address address;
+     // @OneToOne(cascade = CascadeType.ALL)
+     // private Address address;
 
-    public Address getAddress() {
-        return address;
-    }
+     @ManyToMany(targetEntity = Cohort.class)
+     private Set<Cohort> cohort;
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+     public Set<Cohort> getCohort() {
+         return cohort;
+     }
 
+     public void setCohort(Set<Cohort> cohort) {
+         this.cohort = cohort;
+     }
 
-    public Teacher( String salary, String teacherName) {
-        super();
-        this.salary = salary;
-        this.teacherName = teacherName;    }
-    public Teacher() {}
+     //public Address getAddress() {
+     //     return address;
+     // }
 
-    public Teacher(String salary, String teacherName, Department department) {
-        this.salary = salary;
-        this.teacherName = teacherName;
-    }
+     // public void setAddress(Address address) {
+     //     this.address = address;
+     // }
 
+     public Teacher(String salary, String teacherName, Set<Cohort> cohort) {
+         this.salary = salary;
+         this.teacherName = teacherName;
+     }
 
-    public int getTeacherId() {
-        return teacherId;
-    }
-    public void setTeacherId(int teacherId) {
-        this.teacherId = teacherId;
-    }
-    public String getSalary() {
-        return salary;
-    }
-    public void setSalary(String salary) {
-        this.salary = salary;
-    }
-    public String getTeacherName() {
-        return teacherName;
-    }
-    public void setTeacherName(String teacherName) {
-        this.teacherName = teacherName;    }
-}
+     public Teacher(String salary, String teacherName) {
+         super();
+         this.salary = salary;
+         this.teacherName = teacherName;    }
+     public Teacher() {}
+
+     public Teacher(String salary, String teacherName, Department department) {
+         this.salary = salary;
+         this.teacherName = teacherName;
+     }
+
+     public Teacher(String salary, String teacherName) {
+         this.salary = salary;
+         this.teacherName = teacherName;
+     }
+
+     public int getTeacherId() {
+         return teacherId;
+     }
+     public void setTeacherId(int teacherId) {
+         this.teacherId = teacherId;
+     }
+     public String getSalary() {
+         return salary;
+     }
+     public void setSalary(String salary) {
+         this.salary = salary;
+     }
+     public String getTeacherName() {
+         return teacherName;
+     }
+     public void setTeacherName(String teacherName) {
+         this.teacherName = teacherName;    }
+ }
 
